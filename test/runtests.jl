cca5d73f90399ccc77e3460db0b28c6b54735177
Daniel Carrera <dcarrera@gmail.com>
2018-10-19 16:23:02 -0400

    Add tests for isochrones.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,7 @@
 using Test
 using Planets
+using Statistics
 
 include("test_core_radius.jl")
+include("test_isochrone.jl")
+
---
+++ test/test_isochrone.jl
@@ -0,0 +1,24 @@
+tol = 1e-10
+#
+# Sanity check and test grid points.
+#
+df = isochrone(0.1914602816)
+@test all(names(df) .== [:t, :logL, :Te, :logg])
+@test minimum(df[:t]) == 1e5
+@test maximum(df[:t]) == 8.91e8
+
+@test abs(df[1,:t]    - 1e5        ) < tol
+@test abs(df[1,:logL] + 0.209      ) < tol
+@test abs(df[1,:Te]   - 10^(3.5162)) < tol
+@test abs(df[1,:logg] - 2.946      ) < tol
+#
+# Test interpolation.
+#
+m1 = 0.7978261113
+m2 = 0.8456522226
+df = isochrone((m1 + m2)/2)
+
+@test abs(df[1,:logL] - (0.959+1.007)/2       ) < tol
+@test abs(df[1,:Te]   - 10^((3.6480+3.6509)/2)) < tol
+@test abs(df[1,:logg] - (2.925+2.914)/2       ) < tol
+
