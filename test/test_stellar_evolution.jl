c71655a021cb6180357e9af94c77323aecffc424
Daniel Carrera <dcarrera@gmail.com>
2018-12-21 14:35:29 -0500

    Fix test -- rename :Te -> :Teff

--- test/test_stellar_evolution.jl
+++ test/test_stellar_evolution.jl
@@ -9,7 +9,7 @@ df = stellar_evolution(0.1914602816)
 
 @test abs(df[1,:t]    - 1e5        ) < tol
 @test abs(df[1,:logL] + 0.209      ) < tol
-@test abs(df[1,:Te]   - 10^(3.5162)) < tol
+@test abs(df[1,:Teff] - 10^(3.5162)) < tol
 @test abs(df[1,:logg] - 2.946      ) < tol
 #
 # Test interpolation.
@@ -19,6 +19,6 @@ m2 = 0.8456522226
 df = stellar_evolution((m1 + m2)/2)
 
 @test abs(df[1,:logL] - (0.959+1.007)/2       ) < tol
-@test abs(df[1,:Te]   - 10^((3.6480+3.6509)/2)) < tol
+@test abs(df[1,:Teff] - 10^((3.6480+3.6509)/2)) < tol
 @test abs(df[1,:logg] - (2.925+2.914)/2       ) < tol
 
